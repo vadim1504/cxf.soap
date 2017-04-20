dff12bfbdfb0cab76154e117585c3194270e3b7c
Vadim Shestov <vadi-96@mail.ru>
2017-04-20 16:31:42 +0300

    Update RunSoapClient.java

--- src/main/java/com/services/cxf/soap/RunSoapClient.java
+++ src/main/java/com/services/cxf/soap/RunSoapClient.java
@@ -1,39 +1,30 @@
 package com.services.cxf.soap;
 
-import com.cxf.soap.wsdl.services.person.*;
-import com.cxf.soap.wsdl.services.person.ServiceFault;
-import com.cxf.soap.wsdl.types.person.*;
+import com.cxf.soap.wsdl.services.person.ServiceFaultException;
 import com.cxf.soap.wsdl.types.person.Person;
-import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import com.services.cxf.soap.client.PersonClient;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
 
 public class RunSoapClient {
 
     public static void main(String[] args){
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext("com/services/cxf/soap/client");
 
-        String serviceUrl = "http://localhost:9090/cxf/soap/person";
-        JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
-        factory.setServiceClass(PersonPortType.class);
-        factory.setAddress(serviceUrl);
-        PersonPortType personImpl = (PersonPortType) factory.create();
+        PersonClient personClient = annotationConfigApplicationContext.getBean(PersonClient.class);
 
         Person person = new Person();
         person.setName("Vadim");
         person.setBirthday(1996);
 
-        ObjectFactory factory2 = new ObjectFactory();
-        Request request = factory2.createRequest();
-
-        request.setPerson(person);
-        request.setYear(1996);
-
-        Response response = null;
+        List<Person> people = null;
         try {
-            response = personImpl.getFriends(request);
-            response.getFriends().forEach(System.out::println);
+            people = personClient.getFriends(person,1997);
         } catch (ServiceFaultException e) {
-            System.out.printf("Code: "+e.getFaultInfo().getCode()+"; Description: "+e.getFaultInfo().getDescription());
+            e.printStackTrace();
         }
 
-
+        people.forEach(System.out::println);
     }
 }
