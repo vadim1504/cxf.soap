5f77a77fbb0cef8a9815258cb479d32c31969f36
Vadim Shestov <vadi-96@mail.ru>
2017-04-20 15:50:05 +0300

    Update PersonClient.java

--- src/main/java/com/services/cxf/soap/client/PersonClient.java
+++ src/main/java/com/services/cxf/soap/client/PersonClient.java
@@ -1,7 +1,6 @@
 package com.services.cxf.soap.client;
 
 import com.cxf.soap.wsdl.services.person.PersonPortType;
-import com.cxf.soap.wsdl.services.person.ServiceFault;
 import com.cxf.soap.wsdl.services.person.ServiceFaultException;
 import com.cxf.soap.wsdl.types.person.ObjectFactory;
 import com.cxf.soap.wsdl.types.person.Person;
@@ -10,13 +9,15 @@ import com.cxf.soap.wsdl.types.person.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class PersonClient {
 
     @Autowired
     private PersonPortType ClientProxyBean;
 
-    public String getFriends(Person person, int year) throws ServiceFaultException {
+    public List<Person> getFriends(Person person, int year) throws ServiceFaultException {
 
         ObjectFactory factory = new ObjectFactory();
         Request request = factory.createRequest();
@@ -26,9 +27,6 @@ public class PersonClient {
 
         Response response = ClientProxyBean.getFriends(request);
 
-        StringBuilder result = new StringBuilder();
-        response.getFriends().forEach(result::append);
-
-        return result.toString();
+        return response.getFriends();
     }
 }
